1459b3aff34bf1577a0a1bf2217f2c4b076e181a
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2021-02-23 19:08:53 +0200

    Add user management endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+USER_URL = reverse('user:profile')
 
 
 def create_user(**params):
@@ -107,3 +108,58 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """
+            Test that authentication is required for users
+        """
+        response = self.client.get(USER_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """
+        Test API requests that require authentication
+    """
+
+    def setUp(self):
+        self.user = create_user(
+            email='user@mail.com',
+            password='secret',
+            name='Bob'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """
+            Test retrieving profile for logged in user
+        """
+        response = self.client.get(USER_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_profile_not_allowed(self):
+        """
+            Post request is not allowed for profile endpoint
+        """
+        response = self.client.post(USER_URL, {})
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """
+            Test updating the user profile for authenticated user
+        """
+        payload = {'name': 'NewName', 'password': 'new password'}
+
+        response = self.client.patch(USER_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- app/user/urls.py
+++ app/user/urls.py
@@ -8,5 +8,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserAPIView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token')
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('profile/', views.ManageUserView.as_view(), name='profile'),
 ]
